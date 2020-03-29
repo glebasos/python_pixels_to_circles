28e54fa0f9c63bf3889e88d2a4c6419d25bee910
glebasos <47578269+glebasos@users.noreply.github.com>
2020-03-29 16:26:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,29 +8,29 @@ def boxneighbors(a, boxSize, rowNumber, columnNumber): #sliding window to get pi
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID') #video settings
 out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (316,272)) #video settings
-size = 11  # circle diameter
-size = int(size / 2)  # circle radius
-img = cv2.imread("input.png", 1)  #jpg, png, bmp, etc...
+size_d = 4  # circle diameter (4 or more)
+size_r = int(size_d / 2)  # circle radius
+img = cv2.imread("input.png", 1)
 pic = np.copy(img)
 b = pic[..., 0]
-b = np.pad(b, (int(size / 2), int(size / 2)), 'symmetric')
+b = np.pad(b, (size_r, size_r), 'symmetric')
 g = pic[..., 1]
-g = np.pad(g, (int(size / 2), int(size / 2)), 'symmetric')
+g = np.pad(g, (size_r, size_r), 'symmetric')
 r = pic[..., 2]
-r = np.pad(r, (int(size / 2), int(size / 2)), 'symmetric')
+r = np.pad(r, (size_r, size_r), 'symmetric')
 canva = np.copy(pic)
 canva[..., 0] = 0
 canva[..., 1] = 0
 canva[..., 2] = 0
 cv2.namedWindow("output")
 cv2.moveWindow("output", 20, 20)
-for i in range(0, len(pic) - 1, size * 2):
-    for j in range(0, len(pic[i]) - 1, size * 2):
-        pix_b = int(np.mean(boxneighbors(b, size, i + int(size / 2), j + int(size / 2))))
-        pix_g = int(np.mean(boxneighbors(g, size, i + int(size / 2), j + int(size / 2))))
-        pix_r = int(np.mean(boxneighbors(r, size, i + int(size / 2), j + int(size / 2))))
-        center_coordinates = (j + int(size / 2), i + int(size / 2))
-        radius = size
+for i in range(0, len(pic) - 1, size_r * 2):
+    for j in range(0, len(pic[i]) - 1, size_r * 2):
+        pix_b = int(np.mean(boxneighbors(b, size_r, i + size_r, j + size_r)))
+        pix_g = int(np.mean(boxneighbors(g, size_r, i + size_r, j + size_r)))
+        pix_r = int(np.mean(boxneighbors(r, size_r, i + size_r, j + size_r)))
+        center_coordinates = (j + size_r, i + size_r)
+        radius = size_r
         color = (pix_b, pix_g, pix_r)
         thickness = -1
         canva = cv2.circle(canva, center_coordinates, radius, color, thickness)
